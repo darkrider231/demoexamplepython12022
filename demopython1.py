920e98dc234422d37c017a9110fcba207c870585
Grant Patterson <77687606+darkrider231@users.noreply.github.com>
2022-10-12 23:56:58 -0400

    A little demo I did in python3 on my android phone

---
+++ demopython1.py
@@ -0,0 +1,14 @@
+name = input("Enter name:" )
+print("\n")
+age = input("What is your age:" )
+print("\n")
+holiday = input("What is your favorite holiday:")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+
+
+
+print("Your name is " + name + "and you are " + age + " years old. Your favorite holiday is " + holiday + ".")
